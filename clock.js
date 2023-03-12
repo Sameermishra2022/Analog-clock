6783715d6690c46d54849f4daf90b70aefe1b18b
Sameer Mishra <115838087+Sameermishra2022@users.noreply.github.com>
2023-03-12 15:44:18 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+/*let sc = document.querySelector('#sc');
+let mn = document.querySelector('#mn');
+let hr = document.querySelector('#hr');
+
+setInterval(() => {
+console.log(sc,mn,hr);
+let day=new Date();
+let ss=day.getSeconds()*6;
+let mm=day.getMinutes()*6;
+let hh=day.getHours()*30;
+
+sc.style.transform=`rotateZ(${ss}deg)`;
+mn.style.transform=`rotateZ(${mm}deg)`;
+hr.style.transform=`rotateZ(${hh+(mm/12)}deg)`; 
+
+})*/
